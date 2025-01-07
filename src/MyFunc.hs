d2688314956d8d8e5ab2bcfebcfaaa51b27d8bee
inuyasha <yingzijiany@gmail.com>
2025-01-07 17:57:08 +0100

    myfunc update

--- src/MyFunc.hs
+++ src/MyFunc.hs
@@ -1,2 +1,13 @@
 myHead [] = error "error ,empty list kann nicht head machen"
 myHead (x : _) = x
+
+myLength [] = "das ist eine Empty List"
+myLength (x : []) = "das ist eine Elemente List" ++ show x
+myLength (x : y : []) = "das ist zwei Elemente List" ++ show x ++ show y
+myLength (x : y : _) = "das ist lange List mit fst und snd elmente " ++ show x ++ show y
+
+myLength' [] = 0
+myLength' (_ : xs) = 1 + myLength' xs
+
+mySum [] = 0
+mySum (x : xs) = x + mySum xs
