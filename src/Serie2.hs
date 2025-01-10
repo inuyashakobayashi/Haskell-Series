afe34ed82773fe02fc05226845d5c90440327b5a
inuyasha <yingzijiany@gmail.com>
2025-01-10 16:41:44 +0100

    Aufgabe2-4 erledigt

--- src/Serie2.hs
+++ src/Serie2.hs
@@ -67,3 +67,31 @@ doubleSmallNumber x =
   if x > 100
     then x
     else x * 2
+
+-- Aufgabe 2-4
+planet :: Double -> Double -> (Double, Double)
+planet r d = (r, d)
+
+uranus :: (Double, Double)
+uranus = planet 25362000 1270
+
+volume :: (Double, Double) -> Double
+volume (r, d) = 4 / 3 * pi * r ^ 3
+
+mass :: (Double, Double) -> Double
+mass (r, d) = volume (r, d) * d
+
+surfaceArea :: (Double, Double) -> Double
+surfaceArea (r, d) = 4 * pi * r ^ 2
+
+circleArea :: (Double, Double) -> Double
+circleArea (r, d) = pi * r ^ 2
+
+isBigger :: (Double, Double) -> (Double, Double) -> Bool
+isBigger (r1, d1) (r2, d2) = volume (r1, r2) > volume (r2, d2)
+
+isHeavier :: (Double, Double) -> (Double, Double) -> Bool
+isHeavier (r1, d1) (r2, d2) = mass (r1, d1) > mass (r2, d2)
+
+relativeSize :: (Double, Double) -> (Double, Double) -> Double
+relativeSize (r1, d1) (r2, d2) = volume (r1, d1) / volume (r2, d2)
